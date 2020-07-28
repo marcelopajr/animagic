6796e17a60bf27c47907cce50de6e6f9ef2e5c92
Marcelo Pereira <marcelo96jr@gmail.com>
2020-07-28 13:40:21 -0300

    Roteamento e carrossel

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import PageDefault from './components/PageDefault';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 = () => (<PageDefault><h1>Página 404 - Não foi possível localizar nenhum documento</h1></PageDefault>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path='/' component={Home} exact />
+    <Route path='/cadastro/video' component={CadastroVideo} />
+    <Route path='/cadastro/categoria' component={CadastroCategoria} />
+    <Route component={Pagina404} />
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
