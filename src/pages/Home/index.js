3a2e127e1d9b65c4ccb701143ba864a0d1106449
Marcelo Pereira <marcelo96jr@gmail.com>
2020-08-01 22:29:43 -0300

    Home puxando os dados do db.json

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import Footer from '../../components/Footer';
 import categoriasRepository from '../../repositories/categorias';
+import PageDefault from '../../components/PageDefault';
 
 function Home() {
-  useEffect(() => {
-    categoriasRepository.getAllWithVideos();
+  const [dadosIniciais, setDadosIniciais] = useState([]);
 
-    // fetch(URL_BACKEND)
-    //   .then(async (respostaDoServidor) => {
-    //     // const resposta = await respostaDoServidor.json();
-    //     // setCategorias([
-    //     //   ...resposta,
-    //     // ]);
-    //   });
-  });
-  // http://localhost:8080/categorias?_embed=videos
+  useEffect(() => {
+    // http://localhost:8080/categorias?_embed=videos
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        console.log(categoriasComVideos);
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
   return (
-    <div style={{ background: '#141414' }}>
-      <Menu />
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>Carregando...</div>)}
 
-      <BannerMain
+      {dadosIniciais.length >= 1 && (
+      <>
+        <BannerMain
+          videoTitle={dadosIniciais[0].videos[0].titulo}
+          url={dadosIniciais[0].videos[0].url}
+          videoDescription="A descoberta de um novo mundo vai te ensinar a viver a melhor versão de sua vida. Confira o trailer de Soul, nova animação da Disney/Pixar."
+        />
+
+        <Carousel
+          ignoreFirstVideo
+          category={dadosIniciais[0]}
+        />
+      </>
+      )}
+
+      {/* <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription="A descoberta de um novo mundo vai te ensinar a viver a melhor versão de sua vida. Confira o trailer de Soul, nova animação da Disney/Pixar."
@@ -41,10 +58,9 @@ function Home() {
 
       <Carousel
         category={dadosIniciais.categorias[2]}
-      />
+      /> */}
 
-      <Footer />
-    </div>
+    </PageDefault>
   );
 }
 
