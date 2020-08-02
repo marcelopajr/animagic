79e5fbfe4f818c97bac663537807c46e5abf771d
Marcelo Pereira <marcelo96jr@gmail.com>
2020-08-02 18:36:14 -0300

    Cadastro de vídeo implementado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
 import categoriasRepository from '../../repositories/categorias';
 import PageDefault from '../../components/PageDefault';
 
@@ -24,41 +22,32 @@ function Home() {
 
   return (
     <PageDefault paddingAll={0}>
-      {dadosIniciais.length === 0 && (<div>Carregando...</div>)}
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-      {dadosIniciais.length >= 1 && (
-      <>
-        <BannerMain
-          videoTitle={dadosIniciais[0].videos[0].titulo}
-          url={dadosIniciais[0].videos[0].url}
-          videoDescription="A descoberta de um novo mundo vai te ensinar a viver a melhor versão de sua vida. Confira o trailer de Soul, nova animação da Disney/Pixar."
-        />
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription={dadosIniciais[0].videos[0].description}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
 
-        <Carousel
-          ignoreFirstVideo
-          category={dadosIniciais[0]}
-        />
-      </>
-      )}
-
-      {/* <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription="A descoberta de um novo mundo vai te ensinar a viver a melhor versão de sua vida. Confira o trailer de Soul, nova animação da Disney/Pixar."
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
-
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      /> */}
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
 
     </PageDefault>
   );
--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import useForm from '../../../hooks/useForm';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
 
 function CadastroVideo() {
-    return (
-        <PageDefault>
-            <h1>Cadastro de Vídeo</h1>
+  const history = useHistory();
+  const { handleChange, values } = useForm({
+    titulo: 'Teste',
+    url: 'https://youtu.be/3Ec6aZjaZfI',
+    categoria: 'Disney',
+  });
 
-            <Link to='/cadastro/categoria'>
-                Cadastrar Estúdio de Animação
-            </Link>
-        </PageDefault>
-    )
+  return (
+    <PageDefault>
+      <h1>Cadastro de Vídeo</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        // alert('Vídeo cadastrado com sucesso!');
+
+        videosRepository.create({
+          titulo: values.titulo,
+          url: values.url,
+          categoriaId: 2,
+        })
+          .then(() => {
+            console.log('Cadastro efetuado com sucesso!');
+            history.push('/');
+          });
+      }}
+      >
+        <FormField
+          label="Nome do Vídeo"
+          name="titulo"
+          value={values.titulo}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="URL do Vídeo"
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Estúdio de Animação"
+          name="categoria"
+          value={values.categoria}
+          onChange={handleChange}
+        />
+
+        <Button type="submit">
+          Cadastrar
+        </Button>
+      </form>
+
+      <Link to="/cadastro/categoria">
+        Cadastrar Estúdio de Animação
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroVideo;
